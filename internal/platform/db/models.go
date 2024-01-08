28726e7ef3c1b5716425ce301a230988f3052441
Dan Brown <94daniel.brown@gmail.com>
2024-01-08 22:55:17 +0000

    Started adding models

--- internal/platform/db/models.go
+++ internal/platform/db/models.go
@@ -4,6 +4,26 @@ import "github.com/aws/aws-sdk-go-v2/service/dynamodb"
 
 var client *dynamodb.Client
 
+type RoastModels struct {
+	Roast Roast
+}
 
+type ReviewModels struct {
+	Review Review
+}
 
-type
+type Roast struct {
+}
+
+type Review struct {
+}
+
+func NewRoastModels(dynamo *dynamodb.Client) RoastModels {
+	client = dynamo
+	return RoastModels{}
+}
+
+func NewReviewModels(dynamo *dynamodb.Client) ReviewModels {
+	client = dynamo
+	return ReviewModels{}
+}
