2161f2eb88516c0b9a9d0cd3f45e4eaa6725c747
Dan Brown <94daniel.brown@gmail.com>
2024-01-15 21:07:13 +0000

    Started adding CreateRoast

--- cmd/app/handlers.go
+++ cmd/app/handlers.go
@@ -14,7 +14,15 @@ func (app *Config) ListRoasts(c echo.Context) error {
 	return c.JSON(http.StatusOK, []string{"Roast1", "Roast2"}) // Example response
 }
 
-func (app *Config) CreateRoast(c echo.Context) error {
+func (app *Config) CreateRoast(c echo.Context, roastID string, reviewer string, imageUrl, date string) error {
 	// Logic to create a roast
+	roastID = c.FormValue("roastID")
+	reviewer = c.FormValue("reviewer")
+	// Image will be uploaded on frontend and then url passed through to store in dynamo
+	imageUrl = c.FormValue("imageUrl")
 	return c.String(http.StatusCreated, "Roast created")
+}
+
+func (app *Config) GetRoast(c echo.Context) error {
+	return c.JSON(http.StatusOK, "Roast")
 }
