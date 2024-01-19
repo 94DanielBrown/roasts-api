fcd9bd07a73fb1d3f0c05ed498aedd1bff622653
Dan Brown <94daniel.brown@gmail.com>
2024-01-19 22:05:54 +0000

    Fixed error caused by trying to bind data twice

--- cmd/app/validators.go
+++ cmd/app/validators.go
@@ -1,30 +1,48 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/log"
-	"net/http"
+	"io/ioutil"
 	"strings"
-
-	"github.com/94DanielBrown/roc/internal/platform/db"
 )
 
 func (app *Config) CreateRoastValidator(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		var roastReq db.Roast
+		req := c.Request()
 
-		// Bind the JSON payload to the struct
-		if err := c.Bind(&roastReq); err != nil {
-			log.Error(err)
+		// Read the body to a variable
+		body, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			log.Error("Failed to read request body: ", err)
 			return err
 		}
 
-		// Check if roastID is lowercase
-		if roastReq.RoastID != strings.ToLower(roastReq.RoastID) {
-			return c.JSON(http.StatusBadRequest, echo.Map{"error": "roastId must be in lowercase"})
-		}
+		// Log the body
+		log.Info("Request Body: ", string(body))
 
-		c.Set("roastReq", roastReq)
+		req.Body = ioutil.NopCloser(strings.NewReader(string(body)))
+
+		var bodyBytes []byte
+		if c.Request().Body != nil {
+			bodyBytes, _ = ioutil.ReadAll(c.Request().Body)
+			c.Request().Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+			// parse json
+			reqData := struct {
+				RoastID    string `json:"roastId"`
+				PriceRange string `dynamodbav:"SK" json:"priceRange"`
+				Name       string `dynamodbav:"Name" json:"name"`
+				ImageUrl   string `dynamodbav:"ImageUrl" json:"imageUrl"`
+			}{}
+			err := json.Unmarshal(bodyBytes, &reqData)
+			if err != nil {
+				return c.JSON(400, "error json")
+			}
+			fmt.Println(reqData.RoastID)
+		}
 
 		// Call handler
 		return next(c)
